892db28abf01006bcb37e9a76e1b4353b6d4b8e5
karan <adikmp00@gmail.com>
2020-09-22 21:45:32 -0400

    Added CSS styling and added username & time on webpage

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import os
 import flask
 import random
@@ -15,22 +16,32 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 auth_api = API(auth)
 
-dishes = ['French Fries', 'Dosa', 'Caesar Salad', 'Pound Cake', 'Samosa', 'Margherita Pizza', 'Tacos', 'Paneer Tikka']
-tweets = []
-
-dish = dishes[random.randint(0, 7)]
-for tweet in Cursor(auth_api.search, tweet_mode="extended", q=dish + '-filter:retweets', lang="en").items(50):
-    tweets.append(tweet.full_text)
+dishes = ['French Fries', 'Dosa', 'Caesar Salad', 'Pound Cake', 'Samosa', 'Margherita Pizza', 'Tacos',
+              'Paneer Tikka']
 
 app = flask.Flask(__name__, template_folder='./templates')
 
 
 @app.route("/")
 def index():
-    return flask.render_template("index.html", tweets=tweets[random.randint(0, len(dishes)-1)])
+
+    tweets = []
+    username = []
+    created_at = []
+
+    dish = dishes[random.randint(0, 7)]
+    for status in Cursor(auth_api.search, tweet_mode="extended", q=dish + '-filter:retweets', lang="en").items(50):
+        tweets.append(status.full_text)
+        username.append(status.user.screen_name)
+        created_at.append(status.created_at)
+
+    randomSelectInt = random.randint(0, len(dishes) - 1)
+    print(tweets[randomSelectInt])
+    return flask.render_template("index.html", tweets=tweets[randomSelectInt], username=username[randomSelectInt],
+                                 created_at=created_at[randomSelectInt], dish=dish)
 
 
 app.run(
-    port=int(os.getenv('PORT', 5500)),
+    port=int(os.getenv('PORT', 5000)),
     debug=True
 )
