42960896d67b3fc7e0a3ac9522768fac4416d38d
karan <adikmp00@gmail.com>
2020-09-28 14:02:49 -0400

    Added Spoonacular to webpage with ingrediants list

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 import flask
 import random
 import requests
-from flask import request
 from tweepy import OAuthHandler, API, Cursor
 from dotenv import load_dotenv
 
@@ -20,7 +19,7 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 auth_api = API(auth)
 
-dishes = ['French Fries', 'Dosa', 'Caesar Salad', 'Pound Cake', 'Samosa', 'Margherita Pizza', 'Tacos',
+dishes = ['French Fries', 'Dosa', 'Caesar Salad', 'Pound Cake', 'Samosa', 'Pizza', 'Tacos',
           'Paneer Tikka']
 
 app = flask.Flask(__name__, template_folder='./templates')
@@ -32,22 +31,29 @@ def index():
     username = []
     created_at = []
     dish = dishes[random.randint(0, 7)]
-
+    ingredients = []
     content = requests.get("https://api.spoonacular.com/recipes/complexSearch?query=" + dish
-                           + "&number=1&apiKey=" + spoonacular_api)
+                           + "&apiKey=" + spoonacular_api)
     json_response = json.loads(content.text)
-    #print(json_response)
-    image=json_response['results'][0]['image']
+    randomRecipe = random.randint(0, len(json_response['results'])-1)
+    nameRecipe = json_response['results'][randomRecipe]['title']
+    idRecipe = str(json_response['results'][randomRecipe]['id'])
+    imageRecipe = json_response['results'][randomRecipe]['image']
+
+    recipeInfo = requests.get("https://api.spoonacular.com/recipes/" + idRecipe + "/information?includeNutrition=false"
+                                                                                  "&apiKey=" + spoonacular_api)
+    json_response2 = json.loads(recipeInfo.text)
+    for i in range(0, len(json_response2['extendedIngredients'])):
+        ingredients.append(json_response2['extendedIngredients'][i]['originalName'])
 
     for status in Cursor(auth_api.search, tweet_mode="extended", q=dish + '-filter:retweets', lang="en").items(50):
         tweets.append(status.full_text)
         username.append(status.user.screen_name)
         created_at.append(status.created_at)
 
-    randomSelectInt = random.randint(0, len(dishes) - 1)
+    randomSelectInt = random.randint(0, 49)
     return flask.render_template("index.html", tweets=tweets[randomSelectInt], username=username[randomSelectInt],
-                                 created_at=created_at[randomSelectInt], dish=dish, image=image)
-
+                                 created_at=created_at[randomSelectInt], recipe=nameRecipe, image=imageRecipe)
 
 
 app.run(
